4f214e06cecdcf11723967ad8bd8601eb6e09a0f
PatrickMontes <alexandermontes30p@hotmail.com>
2024-10-11 20:22:42 -0500

    UsuarioService listo

--- src/main/java/pe/edu/cibertec/springsecurityt2daw/service/implementation/UsuarioService.java
+++ src/main/java/pe/edu/cibertec/springsecurityt2daw/service/implementation/UsuarioService.java
@@ -1,4 +1,21 @@
 package pe.edu.cibertec.springsecurityt2daw.service.implementation;
 
-public class UsuarioService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pe.edu.cibertec.springsecurityt2daw.model.Usuario;
+import pe.edu.cibertec.springsecurityt2daw.repository.UsuarioRepository;
+import pe.edu.cibertec.springsecurityt2daw.service.interfaces.IUsuarioService;
+
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService implements IUsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    public Usuario getUsuarioPorCodigo(String codigo) {
+        return this.usuarioRepository.findByCodigo(codigo);
+    }
+
 }

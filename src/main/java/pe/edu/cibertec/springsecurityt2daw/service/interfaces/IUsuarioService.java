a36cfee5fdce07867cd23d6327b82a23d3faa17d
PatrickMontes <alexandermontes30p@hotmail.com>
2024-10-11 20:21:31 -0500

    IUsuarioService listo

--- src/main/java/pe/edu/cibertec/springsecurityt2daw/service/interfaces/IUsuarioService.java
+++ src/main/java/pe/edu/cibertec/springsecurityt2daw/service/interfaces/IUsuarioService.java
@@ -1,4 +1,9 @@
 package pe.edu.cibertec.springsecurityt2daw.service.interfaces;
 
+import pe.edu.cibertec.springsecurityt2daw.model.Usuario;
+
 public interface IUsuarioService {
+
+    Usuario getUsuarioPorCodigo(String codigo);
+
 }
